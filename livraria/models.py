1a60b8581c49a4c41bbe5d42176023260b3f9671
MiguelBochiBarros <miguelbochibarro@gmail.com>
2023-06-13 16:29:17 -0300

    Formatação dos arquivos do projeto(exercício 4)

--- livraria/models.py
+++ livraria/models.py
@@ -6,7 +6,8 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
-    
+
+
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
