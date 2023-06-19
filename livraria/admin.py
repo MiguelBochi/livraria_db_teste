fcb47538eba551bfdd3ae87641c8cc2ee5bb5b36
MiguelBochiBarros <miguelbochibarro@gmail.com>
2023-06-19 14:41:10 -0300

    exercicio 6

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora
+from livraria.models import Autor, Categoria, Editora, Livro
 
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
+
