e5549c982c8793f6caa9ebcc29c2ad5c629f186d
MiguelBochiBarros <miguelbochibarro@gmail.com>
2023-06-23 16:05:25 -0300

    exercicio 8

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,14 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet
+from livraria.views import AutorViewSet,CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
+router.register("autores", AutorViewSet)
 router.register("categorias", CategoriaViewSet)
+router.register("editoras", EditoraViewSet)
+router.register("livros", LivroViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- livraria/views.py
+++ livraria/views.py
@@ -1,8 +1,37 @@
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria
-from livraria.serializers import CategoriaSerializer
+from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.serializers import (
+    AutorSerializer,
+    CategoriaSerializer,
+    EditoraSerializer,
+    LivroSerializer,
+    LivroDetailSerializer,
+    LivroListSerializer,
+)
+
+
+class AutorViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = AutorSerializer
+
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
+            return LivroDetailSerializer
+        return LivroSerializer
